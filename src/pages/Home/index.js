c0790ba2fe1fdd80876f86716f644f8d7526737c
Rafael Gonzaga <ralozaga@bol.com.br>
2021-07-11 21:24:40 -0300

    Final da aula 08. Funcionando.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,11 +5,20 @@ import * as S from './styled'
 function App(props) {
   const [ usuario, setUsuario ] = useState('');
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) =>{
+        repositoriesName.push(repository.name);
+      })
+      // console.log(repositoriesName);
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    });
   }
+
   return (
     <S.Container>
-      <p>{usuario}</p>  
+      {/* <p>{usuario}</p>   */}
       <S.Input className='usuarioInput' placeholder='Usuario' value={usuario} onChange={e => setUsuario(e.target.value)}/>
       <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
     </S.Container>
